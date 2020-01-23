ded80bf96c4e4a8c454642820412ec4de024d433
matiassicardi <57410993+matiassicardi@users.noreply.github.com>
2020-01-22 23:46:06 -0300

    Starting this folder

---
+++ building-html-with-js-particles/js-particles/footer.js
@@ -0,0 +1,11 @@
+let footer;
+
+footer =`
+<footer class="py-5 bg-dark">
+    <div class="container">
+      <p class="m-0 text-center text-white">Copyright &copy; Your Website 2019</p>
+    </div>
+    <!-- /.container -->
+  </footer>
+`
+document.getElementById('footer-particle').innerHTML = footer;
---
+++ js-form-validation-checking-things/README.md
@@ -0,0 +1,4 @@
+# js-form-validation-checking-things
+This is an example of a HTML5 Form with a JS Validation. CSS core and JS core are token from BootstrapCDN
+
+This is an example of a HTML5 Form with a JS Validation. CSS core and JS core are token from BootstrapCDN.
