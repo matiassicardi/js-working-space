bd20365a0c0dbe1cdedf68608e3fabd5aa4e5d63
Matias Sicardi <57410993+matiassicardi@users.noreply.github.com>
2020-04-24 16:54:09 -0300

    Ordenando un poco más el código

--- js-agent-language-detector/js-agent-language-detector.js
+++ js-agent-language-detector/js-agent-language-detector.js
@@ -1,12 +1,12 @@
 window.onload = function() {
-            
-var userLang = x=window.navigator.language || navigator.browserLanguage; // detecta la preferencia de idioma del browser
+    if (window.navigator.language != "en" & window.navigator.language !="en-US") {       
+        window.location.href = './es/index.html'; // Envía al sitio en Español 
+        } else {
+            window.location.href = './en/index.html'; // Envía al sitio en Inglés
+        }
+}
 
-if (userLang == 'en' || userLang == 'en_US') {
-    window.location.href = './en/index.html'; // si se detecta el inglés, dirige al sitio en inglés
-    } else if (userLang == 'es') {
-    window.location.href = './es/index.html'; // si se detecta el español, dirige al sitio en español
-    } else {
-    window.location.href = './es/index.html'; // si se detecta cualquier otra preferencia de idioma, dirige al sitio en español
-    }
+window.onload = function() {
+    var idioma = navigator.language || navigator.userLanguage; // Detecta el idioma por defecto del browser
+    alert ("Tu browser está configurado en: " + idioma);
 }
